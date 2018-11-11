05726f4608f9a76f5429162cbc03f72e7a2dc6d4
vicerz <carmor06@ucm.es>
2018-11-11 19:07:12 +0100

    Implementada la búsqueda BFS

--- aima-core/src/main/java/retoatasco/RetoAtascoDemo.java
+++ aima-core/src/main/java/retoatasco/RetoAtascoDemo.java
@@ -8,6 +8,8 @@ import aima.core.search.framework.SearchForActions;
 import aima.core.search.framework.problem.GeneralProblem;
 import aima.core.search.framework.problem.Problem;
 import aima.core.search.framework.qsearch.GraphSearch;
+import aima.core.search.framework.qsearch.TreeSearch;
+import aima.core.search.uninformed.BreadthFirstSearch;
 import aima.core.search.uninformed.DepthFirstSearch;
 import retoatasco.board.RetoAtascoBoard;
 import retoatasco.examples.BasicTrafficJam;
@@ -15,6 +17,7 @@ import retoatasco.examples.BasicTrafficJam;
 public class RetoAtascoDemo {
 	public static void main(String[] args) {
 		atascoDepthFirstSearch();
+		atascoBreadthFirstSearch();
 	}
 	
 	private static void atascoDepthFirstSearch() {
@@ -32,6 +35,24 @@ public class RetoAtascoDemo {
 			printInstrumentation(agent.getInstrumentation());
 		}
 		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void atascoBreadthFirstSearch() {
+		System.out.println("RetoAtascoDemo: BFS --> ");
+		try {
+			Problem<RetoAtascoBoard, AtascoAction> problem = 
+				new GeneralProblem<RetoAtascoBoard, AtascoAction> (new BasicTrafficJam(),
+				RetoAtascoFunctions::getActions, RetoAtascoFunctions::getResult,
+				RetoAtascoFunctions::testGoal);
+			SearchForActions<RetoAtascoBoard, AtascoAction> search = 
+					new BreadthFirstSearch<>(new TreeSearch<>());
+			SearchAgent<RetoAtascoBoard, AtascoAction> agent = 
+					new SearchAgent<>(problem, search);
+			printActions(agent.getActions());
+			printInstrumentation(agent.getInstrumentation());
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
